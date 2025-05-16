679c61b586e75317bdc5559a98ed8f3d341e66ae
Сергій <kjserg@i.ua>
2025-05-16 10:40:59 +0300

    оновив шапку

--- wp-content/themes/adv-mania/header.php
+++ wp-content/themes/adv-mania/header.php
@@ -5,8 +5,6 @@ $logo = carbon_get_theme_option( 'crb_logo' );
 $crb_phone = carbon_get_theme_option( 'crb_phone' );
 $crb_phone_link = carbon_get_theme_option( 'crb_phone_link' );
 $social_links = carbon_get_theme_option( 'social_links' );
-$template_path = '';
-$template_path = get_the_ID() ? get_post_meta(get_the_ID(), '_wp_page_template', true) : '';
 
 ?>
 
@@ -20,9 +18,7 @@ $template_path = get_the_ID() ? get_post_meta(get_the_ID(), '_wp_page_template',
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries-->
     <!-- WARNING: Respond.js doesn't work if you view the page via file://-->
     <!--[if lt IE 9]><script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script><script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-	<?php if($template_path == 'promo-page.php'){
-        echo '<meta name="robots" content="noindex, nofollow">';
-    } ?>
+
     <title><?php wp_title(); ?> </title>
     <?php wp_head(); ?>
 </head>
