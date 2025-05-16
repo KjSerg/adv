74e61d0eb26622c8c8b56c4123409d06a17449b6
Сергій <kjserg@i.ua>
2025-05-16 09:52:26 +0300

    додав promo-page.php

---
+++ wp-content/themes/adv-mania/promo-page.php
@@ -0,0 +1,12 @@
+<?php get_header();
+/*
+ * Template name: Promo-code generator
+ * */
+$var      = variables();
+$set      = $var['setting_home'];
+$assets   = $var['assets'];
+$url      = $var['url'];
+$url_home = $var['url_home'];
+$id       = get_the_ID();
+?>
+<?php get_footer(); ?>
